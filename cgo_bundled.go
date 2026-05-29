8a3e9dc234753f0d9fda82a0d1eb92c1be0686e5
Arun Sharma <arun@sharma-home.net>
2026-05-29 11:00:28 -0700

    Link libm for bundled liblbug on Linux

--- cgo_bundled.go
+++ cgo_bundled.go
@@ -7,7 +7,7 @@ package lbug
 /*
 #cgo CFLAGS: -I${SRCDIR}/lib
 #cgo darwin LDFLAGS: -lc++ -L${SRCDIR}/lib -llbug -Wl,-rpath,${SRCDIR}/lib
-#cgo linux LDFLAGS: -L${SRCDIR}/lib -llbug -lstdc++ -Wl,-rpath,${SRCDIR}/lib
+#cgo linux LDFLAGS: -L${SRCDIR}/lib -llbug -lstdc++ -lm -Wl,-rpath,${SRCDIR}/lib
 #cgo windows LDFLAGS: -L${SRCDIR}/lib -llbug_shared
 #include "lbug.h"
 */
